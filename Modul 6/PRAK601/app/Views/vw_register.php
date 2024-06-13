3711a9192b5111170f2f25b07de9e32aeb64834d
farlyhaydyhdjalil <115335054+farlyhaydyhdjalil@users.noreply.github.com>
2024-06-13 19:31:27 +0800

    commit

--- Modul 6/PRAK601/app/Views/vw_register.php
+++ Modul 6/PRAK601/app/Views/vw_register.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Register · MyLibrary</title>
+    <title>Register</title>
 
     <!-- Custom styles for this template -->
     <style>
